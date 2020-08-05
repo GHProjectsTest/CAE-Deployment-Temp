c90e1dd582ab705970a6642315d1320121143ea1
GHProjectsUser <mail@rwth>
2020-08-05 08:40:32 +0000

    Added microservice 2

---
+++ microservice-2/src/main/i5/las2peer/services/musicService/classes.java
@@ -0,0 +1,40 @@
+package i5.las2peer.services.musicService;
+
+import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.ParseException;
+
+public class classes {
+
+    class Song {
+
+    public Song() {}
+    
+    private String title;
+
+    public void settitle(String setValue) {
+        this.title = setValue;
+    }
+
+    public String gettitle() {
+        return this.title;
+    }
+
+    public JSONObject toJSON() {
+
+        JSONObject jo = new JSONObject();
+        jo.put("title", this.title); 
+
+        return jo;
+    }
+
+    public void fromJSON(String jsonString) throws ParseException {
+        JSONObject jsonObject = (JSONObject) JSONValue.parseWithException(jsonString);
+        this.title = (String) jsonObject.get("title"); 
+
+    }
+
+}
+
+    
+}
